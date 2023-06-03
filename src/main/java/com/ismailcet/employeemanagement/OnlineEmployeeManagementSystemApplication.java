6730a66cf2e03d75e4ecb56b828d3f2162d9885d
ismailcet <ismailcet3@gmail.com>
2023-06-03 15:25:37 +0300

    Project Starter Fİles

---
+++ src/main/java/com/ismailcet/employeemanagement/OnlineEmployeeManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ismailcet.employeemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineEmployeeManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineEmployeeManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ismailcet/employeemanagement/OnlineEmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ismailcet.employeemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineEmployeeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
